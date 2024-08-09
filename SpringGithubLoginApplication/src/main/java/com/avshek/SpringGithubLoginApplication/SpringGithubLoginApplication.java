98249a84121601c4bf1186e33e861fd63291a8d7
Abhishek <abhisam7007871@gmail.com>
2024-08-09 18:11:16 +0530

    Initial Commit

---
+++ SpringGithubLoginApplication/src/main/java/com/avshek/SpringGithubLoginApplication/SpringGithubLoginApplication.java
@@ -0,0 +1,22 @@
+package com.avshek.SpringGithubLoginApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SpringGithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubLoginApplication.class, args);
+	}
+
+
+	@RequestMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("name");
+	}
+}
---
+++ SpringGithubLoginApplication/src/test/java/com/avshek/SpringGithubLoginApplication/SpringGithubLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avshek.SpringGithubLoginApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
